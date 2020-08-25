ff2b78d2c8c875b51572f40edaa056a3746d5cd2
EC2 Default User <ec2-user@ip-172-31-18-85.ap-northeast-1.compute.internal>
2020-08-25 09:34:19 +0000

    fix/post_controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,7 +2,11 @@ class Api::V1::PostsController < ApplicationController
   
   def login_date
     user = User.find_by(email: params[:session][:email].downcase)
-    serializer = UserSerializer.new(user)
-    render json: serializer.serialized_json
+    if user && user.authenticate(params[:session][:password])
+      serializer = UserSerializer.new(user)
+      render json: serializer.serialized_json
+    else
+      render json:{ status: 'failure', message: 'ERROR' }
+    end
   end
 end
