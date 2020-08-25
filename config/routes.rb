08a0f805213d0970b50a58a5d86381e400f6b741
EC2 Default User <ec2-user@ip-172-31-18-85.ap-northeast-1.compute.internal>
2020-08-25 11:33:54 +0000

    fix/route

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :posts do
-        get :login_date, on: :collection
+        post :login_date, on: :collection
       end
     end
   end
