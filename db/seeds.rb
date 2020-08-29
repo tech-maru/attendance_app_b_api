fa602c0823462207660d6002559f3e5c25b3da4e
EC2 Default User <ec2-user@ip-172-31-18-85.ap-northeast-1.compute.internal>
2020-08-29 12:42:15 +0000

    seed/attendance

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.create!(name: "Admin User",
              password_confirmation: "sample1@email.com",
              admin: "true")
 
-60.times do |n|
+10.times do |n|
   name  = Faker::Name.name
   email = "sample-#{n+2}@email.com"
   password = "sample-#{n+2}@email.com"
@@ -15,4 +15,31 @@ User.create!(name: "Admin User",
                password: password,
                password_confirmation: password,
                admin: "false")
+               
 end
+
+puts "CREATE 10USER"
+               
+# attendance
+
+user = User.find(2)
+
+typeA = ["02", "04", "06", "10", "12"]
+typeB = ["01", "03", "05", "07", "09", "11", "15"]
+
+typeB.each do |number|
+  user.attendances.create!(
+    worked_on: "2020-08-#{number}",
+    started_at: "2020-08-#{number} 10:00:00",
+    finished_at: "2020-08-#{number} 18:00:00",
+  )
+end
+
+typeA.each do |number|
+  user.attendances.create!(
+    worked_on: "2020-07-#{number}",
+    started_at: "2020-07-#{number} 10:00:00",
+    finished_at: "2020-07-#{number} 18:00:00",
+  )
+end
+
