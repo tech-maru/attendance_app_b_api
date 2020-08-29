780368ac9680042f6cf310ed9363df977e35f603
EC2 Default User <ec2-user@ip-172-31-18-85.ap-northeast-1.compute.internal>
2020-08-29 12:12:12 +0000

    create/api_attendance

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -10,4 +10,25 @@ class Api::V1::PostsController < ApplicationController
       render json:{ status: 'failure', message: 'ERROR' }
     end
   end
+  
+  def pick_attendance
+    user = User.find(params[:id])
+    object_month
+    if @attendances = user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
+      serializer = AttendanceSerializer.new(attendance)
+      render json: serializer.serialized_json
+    else
+      render json:{ status: 'failure', message: 'ERROR' }
+    end
+  end
+  
+  private
+  
+  def object_month
+    @first_day = params[:date].nil? ?
+    Date.current.beginning_of_month : params[:date].to_date
+    @last_day = @first_day.end_of_month
+    @one_week = [*@first_day..@last_day]
+  end
+  
 end
