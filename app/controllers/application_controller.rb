27497352c897c80da90bc0ea0fb2ba06e0a6a3b1
EC2 Default User <ec2-user@ip-172-31-18-85.ap-northeast-1.compute.internal>
2020-08-31 08:21:39 +0000

    delete/attendance_seli

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,16 +46,10 @@ class ApplicationController < ActionController::Base
     @first_day = params[:date].nil? ?
     Date.current.beginning_of_week : params[:date].to_date
     @last_day = @first_day.end_of_week
-    one_week = [*@first_day..@last_day]
+    @one_week = [*@first_day..@last_day]
     
     @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     
-    unless one_week.count == @attendances.count
-      ActiveRecord::Base.transaction do
-        one_week.each { |day| @user.attendances.create!(worked_on: day)}
-      end
-      @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
-    end
     
   rescue ActiveRecord::RecordInvalid
     flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
--- app/models/attendances_serializer.rb
+++
@@ -1,6 +0,0 @@
-class AttendancesSerializer
-  include FastJsonapi::ObjectSerializer
-
-  attributes :attendances[]
-  
-end
