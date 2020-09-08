3ca9647ed8381a8984dda8e8e82693e84111a946
EC2 Default User <ec2-user@ip-172-31-18-85.ap-northeast-1.compute.internal>
2020-09-08 17:12:25 +0000

    fix/user_create/failure

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -59,7 +59,7 @@ class Api::V1::PostsController < ApplicationController
       serializer = UserSerializer.new(user)
       render json: serializer.serialized_json
     else
-      render json:{ status: 'failure', message: user.errors }
+      render json:{ status: 'failure', message: user.errors.messages }
     end
   end
 
